b5f787085c3776c527164e5cad54a5c5b0a98dcb
sg6215 <sg6215@ic.ac.uk>
2016-12-06 12:34:08 +0000

    Extracted IO handling without comments

--- Main.cpp
+++ Main.cpp
@@ -29,38 +29,6 @@ int main(int argc, char* args[]) {
   // Textures
   load();
   io.run(m_renderer, bob_c, t_container);
-
-  //bool quit = false;
-  //SDL_Event e;
-  //while (!quit) {
-  //  // Handle events on queue
-  //  while (SDL_PollEvent(&e) != 0) {
-  //    if (e.type == SDL_QUIT) {
-  //      quit = true;
-  //    } else if (e.type == SDL_KEYDOWN) {
-  //      // Select surfaces based on key press
-  //      switch (e.key.keysym.sym) {
-  //        case SDLK_UP:
-  //        case SDLK_SPACE:
-  //          bob_c->set_jump();
-  //          break;
-  //        default:
-  //          break;
-  //      }
-  //    }
-  //  }
-
-  //  // Clear screen
-  //  SDL_SetRenderDrawColor(m_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-  //  SDL_RenderClear(m_renderer);
-
-  //  t_container->run(m_renderer);
-  //  bob_c->run(m_renderer);
-
-  //  // Update screen
-  //  SDL_RenderPresent(m_renderer);
-  //}
-
   // Free resources and close SDL
   close();
   return 0;
