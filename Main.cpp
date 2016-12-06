5e7e44ecae2cc4b5289f0b632c3d623ea0c9bd2a
sg6215 <sg6215@ic.ac.uk>
2016-12-06 12:15:49 +0000

    Extracted bg without comments

--- Main.cpp
+++ Main.cpp
@@ -25,9 +25,6 @@ SDL_Renderer* m_renderer = NULL;
 SDL_Renderer& rendererRef = *m_renderer;
 std::shared_ptr<Bob> bob_c;
 std::shared_ptr<TextureContainer> t_container;
-//std::shared_ptr<Texture> t_bg;
-//std::shared_ptr<Texture> t_floor;
-//std::vector<std::shared_ptr<Texture>> texture_vector;
 
 void close() {
   bob_c->clear();
@@ -46,10 +43,6 @@ void close() {
 void load() {
   bob_c = std::make_shared<Bob>(m_renderer);
   t_container = std::make_shared<TextureContainer>(m_renderer);
-  //t_bg = std::make_shared<Texture>("assets/bg2.png", m_renderer);
-  //t_floor = std::make_shared<Texture>("assets/floor.png", m_renderer);
-  //texture_vector.push_back(t_bg);
-  //texture_vector.push_back(t_floor);
 }
 
 int main(int argc, char* args[]) {
@@ -65,8 +58,6 @@ int main(int argc, char* args[]) {
   bool quit = false;
 
   SDL_Event e;
-
-  //int bob_height = (SCREEN_HEIGHT - 120) - 80 + 10;
 
   int bg_scroller_div = 5;
   int floor_scroller_div = 4;
@@ -95,19 +86,11 @@ int main(int argc, char* args[]) {
     SDL_SetRenderDrawColor(m_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(m_renderer);
 
-//    t_bg->render((-1 * bg_scroller_offset * bg_scroller_div), 0, m_renderer);
-//    t_floor->render((-1 * bg_scroller_offset * floor_scroller_div),
-//                    (SCREEN_HEIGHT - 120), m_renderer);
     t_container->run(m_renderer);
     bob_c->run(m_renderer);
 
     // Update screen
     SDL_RenderPresent(m_renderer);
-//    bg_scroller_offset++;
-//    bg_scroller_offset %= (SCREEN_WIDTH / bg_scroller_div);
-//    floor_scroller_offset++;
-//    floor_scroller_offset %= (SCREEN_WIDTH / floor_scroller_div);
-
   }
 
   // Free resources and close SDL
